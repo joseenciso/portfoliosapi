64c4e7bf7ee1b28320aa6d720178cbf6b2afebfe
Jose Enciso <josejefloen@gmail.com>
2025-05-12 21:31:52 +0100

    Correction for CORS and first GET

--- index.js
+++ index.js
@@ -26,9 +26,12 @@ let projects = [
 
 
 app.use(express.json(),(req, res, next) => {
-    res.header("Access-Controll-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 })
-//app.use(express.json());
+
+app.use(express.json());
 
 // Get all projects
 app.get('/projects', (req, res) => {
